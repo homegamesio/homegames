586b7ae9757da992a2fd589d175ac2a295a38121
Joseph Garcia <prosif@gmail.com>
2021-01-13 22:00:29 -0700

    actually fix bundle

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const path = require('path');
 const hgCorePath = path.join(__dirname, 'node_modules/homegames-core');
 const hgWebPath = path.join(__dirname, 'node_modules/homegames-web');
 
+const webBundlePath = path.join(__dirname, 'node_modules/homegames-web/web/bundle.js');
+
 fork(`${hgCorePath}/index.js`);
 fork(`${hgWebPath}/index.js`);
