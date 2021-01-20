0401cd4f66d109978acd592712b4d2b9c7c2ce7f
Joseph Garcia <prosif@gmail.com>
2021-01-19 23:25:14 -0700

    wip - https almost working

--- index.js
+++ index.js
@@ -10,4 +10,8 @@ const hgWebPath = path.join(__dirname, 'node_modules/homegames-web');
 const webBundlePath = path.join(__dirname, 'node_modules/homegames-web/web/bundle.js');
 
 fork(`${hgCorePath}/index.js`);
-fork(`${hgWebPath}/index.js`);
+
+console.log('starting web server in 20 seconds');
+setTimeout(() => {
+    fork(`${hgWebPath}/index.js`);
+}, 20 * 1000);
