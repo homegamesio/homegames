121168f7335d2f7660f4acdebe6df46d6a2e0204
Joseph Garcia <prosif@gmail.com>
2021-01-22 17:20:24 -0700

    0.5.6 - reduced server start time by 20 secondsgs

--- index.js
+++ index.js
@@ -10,8 +10,4 @@ const hgWebPath = path.join(__dirname, 'node_modules/homegames-web');
 const webBundlePath = path.join(__dirname, 'node_modules/homegames-web/web/bundle.js');
 
 fork(`${hgCorePath}/index.js`);
-
-console.log('starting web server in 20 seconds');
-setTimeout(() => {
-    fork(`${hgWebPath}/index.js`);
-}, 20 * 1000);
+fork(`${hgWebPath}/index.js`);
