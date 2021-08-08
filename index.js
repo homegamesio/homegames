83ee5184d9c093a4b0165b537bffa5586b6d1502
Joseph Garcia <prosif@gmail.com>
2021-08-08 15:51:46 -0700

    i am a fool

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const { fork } = require('child_process');
 const path = require('path');
 const process = require('process');
+const squish061 = require('squish-061');
+const squish063 = require('squish-063');
+const squish0631 = require('squish-0631');
+const squish0632 = require('squish-0632');
+const squish0633 = require('squish-0633');
 
 process.env.LINK_ENABLED = true;
 
