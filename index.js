b45f0cd59eb9feb534270fa9eb58c6e8b7423a4a
Joseph Garcia <prosif@gmail.com>
2022-06-09 21:07:20 -0700

    0.7.54

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const { fork } = require('child_process');
 const path = require('path');
 const process = require('process');
-const squish061 = require('squish-061');
-const squish063 = require('squish-063');
-const squish0631 = require('squish-0631');
-const squish0632 = require('squish-0632');
-const squish0633 = require('squish-0633');
 
 process.env.LINK_ENABLED = true;
 
@@ -14,13 +9,5 @@ const hgWebPath = path.join(__dirname, 'node_modules/homegames-web');
 
 const webBundlePath = path.join(__dirname, 'node_modules/homegames-web/web/bundle.js');
 
-const squishMap = {
-    'squish-061': require.resolve('squish-061'),//squish061,
-    'squish-063': require.resolve('squish-063'),
-    'squish-0631': require.resolve('squish-0631'),
-    'squish-0632': require.resolve('squish-0632'),
-    'squish-0633': require.resolve('squish-0633')
-};
-
-fork(`${hgCorePath}/index.js`, [JSON.stringify(squishMap)]);
-fork(`${hgWebPath}/index.js`, [JSON.stringify(squishMap)]);
+fork(`${hgCorePath}/index.js`);
+fork(`${hgWebPath}/index.js`);
