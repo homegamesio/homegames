460cfa3c9c6382f60822cf448f5e89911dce1c2e
Joseph Garcia <prosif@gmail.com>
2024-11-24 16:20:03 -0700

    ayo

--- upload_builds.sh
+++ upload_builds.sh
@@ -1,3 +1,6 @@
 aws s3 cp homegames-arm64.dmg s3://builds.homegames.io/stable/homegames-arm64.dmg
+aws s3 cp homegames-x64.dmg s3://builds.homegames.io/stable/homegames-x64.dmg
+aws s3 cp homegames-x64.snap s3://builds.homegames.io/stable/homegames-x64.snap
+aws s3 cp homegames-setup-x64.exe s3://builds.homegames.io/stable/homegames-setup-x64.exe
 
 aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --paths "/*"
